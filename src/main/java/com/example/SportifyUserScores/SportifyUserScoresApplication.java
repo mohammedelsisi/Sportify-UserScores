3a84833b21f4d43cdce6c19a2bad2e6986a7874a
m.elsisi <mohammed.elsisi550@gmail.com>
2021-06-06 20:25:51 +0200

    first commit

---
+++ src/main/java/com/example/SportifyUserScores/SportifyUserScoresApplication.java
@@ -0,0 +1,13 @@
+package com.example.SportifyUserScores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportifyUserScoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportifyUserScoresApplication.class, args);
+	}
+
+}
