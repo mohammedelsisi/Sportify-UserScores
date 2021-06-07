6b0f1416c2e0f207fdfb9ae6fad31d872e158c4f
yasmina <yasminaabdelazim@gmail.com>
2021-06-07 04:11:20 +0200

    add validation filter

--- src/main/java/com/example/SportifyUserScores/controller/ValidatingEndPoint.java
+++ src/main/java/com/example/SportifyUserScores/controller/ValidatingEndPoint.java
@@ -1,4 +1,20 @@
 package com.example.SportifyUserScores.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.SportifyUserScores.util.JwtUtil;
+
+@RestController
 public class ValidatingEndPoint {
+
+    @Autowired
+    final JwtUtil jwtUtil;
+    String jwt = null;
+    String email= null;
+
+    @RequestMapping("/hello")
+    public String hello(){
+        email = jwtUtil.extractEmail(jwt);
+        return email; }
 }
--- src/main/java/com/example/SportifyUserScores/filters/JwtRequestFilter.java
+++ src/main/java/com/example/SportifyUserScores/filters/JwtRequestFilter.java
@@ -14,14 +14,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+
 @ComponentScan
 public class JwtRequestFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private MyUserDetailsService userDetailsService;
+//    @Autowired
+//    private MyUserDetailsService userDetailsService;
 
-    @Autowired
-    private JwtUtil jwtUtil;
+    //@Autowired
+    //private JwtUtil jwtUtil;
 
 
     @Override
@@ -32,20 +34,20 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         String username = null;
         String jwt = null;
 
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
             username = jwtUtil.extractUsername(jwt);
         }
 
-        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-            if (jwtUtil.validateToken(jwt, userDetails)){
+            if (jwtUtil.validateToken(jwt, userDetails)) {
 
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails , null, userDetails.getAuthorities());
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
-        chain.doFilter(request,response);
+        chain.doFilter(request, response);
     }
 }
